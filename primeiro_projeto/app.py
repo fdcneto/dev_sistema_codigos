ae40dbdf980fee771bbb01faa381178f401fdc04
Francisco <fdcneto@outlook.com>
2025-10-10 19:24:36 -0300

    tudo

--- primeiro_projeto/app.py
+++ primeiro_projeto/app.py
@@ -1,41 +1,133 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 # importando a Class Flask do modulo flask
 
 # criar uma instancia do Flask = objeto
 app = Flask(__name__)
-#__name__ é o nome do modulo atual -> navegação
+# __name__ é o nome do modulo atual -> navegação
+
+# Essa linha permite usar caracteres especiais
+# no JSON
+app.config['JSON_AS_ASCII'] = False
+# se esse de cima não pegar, use esse abaixo
+# app.json.ensure_ascii = False
+
+usuarios = [
+    {'id': 1, 'nome': 'David', 'email': 'davidvarao@senai.br'},
+    {'id': 2, 'nome': 'Geovanni', 'email': 'pizza35reais@senai.br'}
+]
 
 # Definir Rotas (Endpoints)
-# quando acessar o servidor, chame essa função
+# quando acessar ao servidor, chame essa função
 @app.route('/')
 def home():
     return 'Welcome to the mundo das APIs'
 
-@app.route('/sobre')
-def sobre():
-    return 'Estamos na aula de DEV de Sistemas. Estamos aprendendo API com Flask'
+# Sempre faça uma função/rota de verificação
+# do status da API
+@app.route('/status')
+def status():
+    return jsonify({
+        'status':'online',
+        'mensagem':'API funcionando corretamente'
+    })
 
 @app.route('/contato')
 def contato():
-    return '21 4002-8922\n' \
+    return '21 4002-8922\n ' \
     'email: ta_dificil@dev_sistemas.senai.br'
 
-@app.route('/usuario/<nome>')
-def ususario(nome):
-    return f'Prezado,{nome}', 'espero que essa mensagem o encontre bem'
+# Rota com GET -> Listar todos os usuarios
+@app.route('/usuarios', methods=['GET'])
+def listar_usuarios():
+    return jsonify(usuarios), 200
 
-# Rota com parametro numérico
+@app.route('/usuario/<int:id>', methods=['GET'])
+def buscar_usuario(id):
+    # Poderia ser reescrito assim
+    # u = None
+    # for usuario in usuarios:
+    #     if usuario['id'] == id:
+    #         u = usuario
+
+    # compreensão de listas
+    u = [usuario for usuario in usuarios if usuario['id']==id]
+    
+    if u == []:
+        return jsonify({
+            'erro': "Dados inválidos"
+        }), 404
+    else:
+        return jsonify(u), 200
+
+# POST -> Ideia de Criação
+@app.route('/usuarios', methods=['POST'])
+def criar_usuario():
+    # pegar dados do frontend/requisição
+    dados = request.get_json()
+
+    # verificar se há algo no JSON
+    if not dados:
+        return jsonify({
+            'erro':'Não há Dados'
+        }), 400
+    
+    # Pegar um campo do JSON
+    nome = dados.get('nome', '').strip()
+
+    # Isso não ve se esta vazio, 
+    # verifica se existe
+    if not nome:
+        return jsonify({
+            'erro':'Não há nome'
+        }), 400
+    
+    # Verificar se tem algo escrito
+    if len(nome) < 1:
+        return jsonify({
+            'erro':'Nome Inválido'
+        }), 400
+
+    # lower -> tudo em minuscula
+    email = dados.get('email', '').strip().lower()
+
+    if not email:
+        return jsonify({
+            'erro':'Não há email'
+        }), 400
+    
+    email_existente = [usuario for usuario in usuarios 
+            if usuario['email'] == email]
+    
+    if email_existente == []:
+        id = len(usuarios) + 1
+        novo_usuario = {
+            'id':id,
+            'nome': nome,
+            'email':email
+        }
+        usuarios.append(novo_usuario)
+        return jsonify({
+            'status':201,
+            'mensagem': 'Novo usuário criado'
+        })
+    else:
+        return jsonify({
+            'status':400,
+            'mensagem': 'Email já existente'
+        })
+
+
+# Rota com parâmetro numérico
 @app.route('/produto/<int:id>')
+def produto(id):
+    return f'Você escolheu o produto com id:{id}'
 
-def produto (id):
-    return f' Você escolheu o produro com {id}'
+# flask ele converte string em um resposta HTTP
 
-# o flask converte string em uma resposta HTTP
-
-# EXECUTAR O SERVIDOR
+# executar o servidor
 if __name__ == '__main__':
     app.run(debug=True)
-    # debug -> servidor no modo de desenvolvimento
-    # ativa o reload automático e mostra
-    # os erros de forma detalhada
-
+# debug -> servidor no modo de desenvolvimento
+# ativa o reload automatico e mostra
+# os erros de forma detalhada
+# TESTE
