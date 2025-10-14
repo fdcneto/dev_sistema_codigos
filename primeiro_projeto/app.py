89c9bae2c8c70bbad7babe53d1b04bb50bbee22d
Francisco Das Chagas Neto <ART0027632@aluno.firjansenaisesi.com.br>
2025-10-14 19:19:58 -0300

    restaurando arquivos

--- primeiro_projeto/app.py
+++ primeiro_projeto/app.py
@@ -41,6 +41,10 @@ def validar_usuario(dados):
         "Campo 'nome' é obrigatorio", None
     # ja que o campo existe, iremos pegar o 
     # valor associado a ele
+    if not isinstance(dados['nome'], str):
+        return False, "O campo 'nome' deve \
+            ser textual", None
+
     nome = dados.get('nome', '').strip()
 
     # tenho o nome, o que eu quero validar?
@@ -59,23 +63,55 @@ def validar_usuario(dados):
         return False, \
     "Campo 'email' é obrigatório", None
 
+    if not isinstance(dados['email'], str):
+        return False, "O campo 'email' deve \
+            ser textual", None
+    
+    if ('@' not in dados['email'] or 
+    '.' not in dados['email']):
+        return False, "O email informado não \
+            é válido", None
+
     email = dados.get('email', '').strip().lower()
 
     if not email:
         return False, "Campo 'email' não \
         pode estar vazio", None
-
-    # Certeza -> Campo email existe e o 
+    
+    # Certeza -> Campo Email existe e o 
     # email existe
     email_existente = [user for user in usuarios if email == user['email']]
 
     # verificar se há algum usuario com esse email
     if len(email_existente) > 0:
-        return False, "Este email já está cadastrado" , None
+        return False, "Este email já \
+            está cadastrado", None
+    
+    #  ------------------------------------
+    if 'senha' not in dados:
+        return False, "Campo 'senha'\
+              é Obrigatorio", None
+    
+    # senha ja existe
+    senha = dados.get('senha', '').strip()
+
+    if not senha:
+        return False, "Campo 'senha' não pode \
+            estar vazio", None
+    
+    if len(senha) < 8 and len(senha) > 50:
+        return False, "Campo 'senha' com \
+            tamanho inválido", None 
+    
+    dados_validados = {
+        'nome': nome,
+        'email': email,
+        'senha': senha
+    }
+    return True, None, dados_validados
 
 
-
-
+     
 
 # Rota padrão - Index - Landpage
 @app.route('/')
@@ -108,8 +144,79 @@ def criar_usuario():
     valido, erro, dados_validados = \
     validar_usuario(request.get_json())
 
+    # if valido == False é a mesma coisa que:
+    if not valido:
+        return jsonify({
+            'erro': erro
+        })
 
+    novo_usuario = {
+        'id': proximo_id,
+        'nome': dados_validados['nome'],
+        'email': dados_validados['email'],
+        'senha': dados_validados['senha']
+    }
+    usuarios.append(novo_usuario)
+    # preparar proxima inserção
+    proximo_id += 1
 
+    # já que não vamos mais usar novo_usuario
+    # removeremos a senha dele para apresentar
+    novo_usuario.pop('senha', None)
+
+    return jsonify({
+        'mensagem':'Usuário criado com sucesso',
+        'usuario': novo_usuario
+    }), 201
+
+# LOGIN
+@app.route('/login', methods=['POST'])
+def login():
+    dados = request.get_json()
+
+    if not dados:
+        return jsonify({
+            'erro':'Corpo da requisição não \
+            pode estar vazio'
+        }), 400
+    
+    email = dados.get('email', '').strip().lower()
+    senha = dados.get('senha', '').strip()
+
+    if not email or not senha:
+        return jsonify({
+            'erro':'Os campos de email e senha \
+                são obrigatórios'
+        }), 400
+
+    usuario = [user for user in usuarios
+               if email == user['email']]
+    
+    usuario = usuario[0]
+    if len(usuario) < 1:
+        return jsonify({
+            'erro':'Email ou senha inválidos'
+        }), 400
+    
+    print(usuario)
+    if senha != usuario['senha']:
+        return jsonify({
+            'erro':'Email ou senha inválidos'
+        }), 400
+    
+    usuario.pop('senha', None)
+    return jsonify({
+        'mensagem':'Login realizado com sucesso',
+        'usuario':usuario,
+        'token': f"token_user_{usuario['id']}"
+    }), 200
+
+    # verificar
+    # 1. Se há dados
+    # 2. verificar se há email e senha
+    # 3. Verificar se o usuario 
+    # (email ou nome, vc escolhe) esta cadastrado
+    # 4. verficiar se a senha esta igual
 
 
 # iniciar o servidor -> porta padrão 5000
